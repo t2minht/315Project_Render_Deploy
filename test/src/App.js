fdffb747ec6ed39beba7f98f696a171622e5f9f2
Michael Mirhosseini <mmmirhosseini@tamu.edu>
2022-11-21 12:33:55 -0600

    Sprint 2

--- test/src/App.js
+++ test/src/App.js
@@ -9,6 +9,16 @@ import ManagerHome from "./ManagerPages/home.js";
 import RestockReport from "./ManagerPages/restockReport.js";
 import ExcessReport from "./ManagerPages/excessReport.js";
 import SalesTogether from "./ManagerPages/salesTogether.js";
+import Checkout from "./pages/checkout.js";
+import Drizzle from "./pages/drizzle.js";
+import Meats from "./pages/meats.js";
+import Pizzatype from "./pages/pizzatype.js";
+import Sauce from "./pages/sauce.js";
+import Seasonal from "./pages/seasonal.js";
+import Topping from "./pages/topping.js";
+import Veggies from "./pages/veggies.js";
+import Directions from "./pages/directions.js"
+
 
 function App() {
 
@@ -92,6 +102,33 @@ function App() {
             break
         case "/salesTogether":
             component = <SalesTogether data={togetherTable} column={toColumn}/>
+            break
+        case "/pizzatype":
+            component = <Pizzatype />
+            break
+        case "/topping":
+            component = <Topping />
+            break
+        case "/veggies":
+            component = <Veggies />
+            break
+        case "/drizzle":
+            component = <Drizzle />
+            break
+        case "/meats":
+            component = <Meats />
+            break
+        case "/sauce":
+            component = <Sauce />
+            break
+        case "/seasonal":
+            component = <Seasonal />
+            break
+        case "/checkout":
+            component = <Checkout />
+            break
+        case "/directions":
+            component = <Directions />
             break
     }
     return (
