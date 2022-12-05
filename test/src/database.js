eaf6ddee6e391b011d5e0431050dc4889be56e1a
Michael Mirhosseini <mmmirhosseini@tamu.edu>
2022-12-05 12:13:55 -0600

    removed second pool

--- test/src/database.js
+++ test/src/database.js
@@ -20,7 +20,7 @@ const pool = new Pool({
     database: process.env.PSQL_DATABASE,
     password: process.env.PSQL_PASSWORD,
     port: process.env.PSQL_PORT,
-    ssl: {rejectUnauthorized: false}
+    ssl: { rejectUnauthorized: false }
 });
 const menuCols = ["ID", "Name", "Price"]
 const inventoryCols = ["ID", "Name", "Item Type", "Count", "Cost", "Total Sales"]
@@ -64,57 +64,57 @@ app.get("/pizza/:itemName", async (req, res) => {
 app.get("/menu", async (req, res) => {
     let inv = await pool.query('SELECT * FROM menu');
     res.json(inv.rows);
-    
+
 });
 
 app.get("/restockReport", async (req, res) => {
     let inv = await pool.query('SELECT * FROM inventory WHERE count < 100');
     res.json(inv.rows);
-    
+
 });
 
 app.get("/excessReport/:beginDate/:endDate", async (req, res) => {
     //console.log(req.params);
     let inv = await pool.query("SELECT id FROM customerinfo WHERE orderdate BETWEEN '" + req.params.beginDate + "' and '" + req.params.endDate + "'");
     res.json(inv.rows);
-    
+
 });
 
 
 app.post("/addIngredient", async (req, res) => {
     console.log(inventory.length);
-    let inv = await pool.query("INSERT INTO inventory VALUES (" + req.body.num + ", '"+ req.body.name + "', " + "'Seasonal', " + req.body.count + ", " + req.body.cost + ", 0)");
+    let inv = await pool.query("INSERT INTO inventory VALUES (" + req.body.num + ", '" + req.body.name + "', " + "'Seasonal', " + req.body.count + ", " + req.body.cost + ", 0)");
     res.json(inv.rows[0]);
-    
+
 });
 //TODO: GATHER TEXT INPUTS FOR ADDING MENU ITEMS TO THE MENU, ITEMS TO INVENTORY, AND SEASONAL ITEMS, AND TO CHANGE PRICE
 
 
 app.post("/addItem", async (req, res) => {
-    let inv = await pool.query("INSERT INTO menu VALUES(" + req.body.id+ ",'" + req.body.name + "',"+ req.body.price +")");
+    let inv = await pool.query("INSERT INTO menu VALUES(" + req.body.id + ",'" + req.body.name + "'," + req.body.price + ")");
     res.json(inv.rows[0]);
-    
+
 });
 
 app.put("/updatePrice", async (req, res) => {
     let inv = await pool.query("UPDATE menu SET price = " + req.body.newPrice + " WHERE id =" + req.body.id);
     res.json(inv.rows[0]);
-    
+
 });
 
 app.put("/updateAmount", async (req, res) => {
     var id1 = req.body.id;
     var newAmount = req.body.newAmount;
     let inv = await pool.query("UPDATE inventory SET count = count+" + newAmount + " WHERE id ="
-    + id1 + "RETURNING *");
+        + id1 + "RETURNING *");
     res.json(inv.rows[0]);
-    
+
 });
 
 app.get("/salesTogether/:beginDate/:endDate", async (req, res) => {
     let inv = await pool.query("SELECT customerinfo.id,customerinfo.orderdate,pizza.pizzaname,drinks.drinkname FROM customerinfo INNER JOIN drinks ON customerinfo.id=drinks.id INNER JOIN pizza ON customerinfo.id=pizza.id WHERE orderdate BETWEEN '" + req.params.beginDate + "' and '" + req.params.endDate + "'");
     res.json(inv.rows);
-    
+
 });
 
 let pizza = {
@@ -407,16 +407,6 @@ app.get('/currentPizza', function (req, res) {
     price = price.toFixed(2);
     thisPizza += price;
     res.json(thisPizza);
-});
-
-//Make the pool for later use
-const pool = new Pool({
-    user: process.env.PSQL_USER,
-    host: process.env.PSQL_HOST,
-    database: process.env.PSQL_DATABASE,
-    password: process.env.PSQL_PASSWORD,
-    port: process.env.PSQL_PORT,
-    ssl: { rejectUnauthorized: false }
 });
 
 app.put("/checkoutServ", async (req, res) => {
