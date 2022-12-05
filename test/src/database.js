37fd2920bbe5f511ffb2cad3752b88137eae6bc8
Michael Mirhosseini <mmmirhosseini@tamu.edu>
2022-12-05 12:09:26 -0600

    updating backend

--- test/src/database.js
+++ test/src/database.js
@@ -124,25 +124,44 @@ let pizza = {
     numToppings: 0,
     currToppings: 0,
     toppings: [],
-    isCauly: false,
-    isCombo: false
+    isCauly: "false",
+    isCombo: "false",
+    price: 0
 }
 
 var pizzaList = [];
+var numDrinks = 0;
+
+app.get('/currentToppings', function (req, res) {
+    var allToppings = "";
+    for (let i = 0; i < pizza.currToppings; i++) {
+        allToppings += pizza.toppings[i];
+        allToppings += ",";
+    }
+    res.json(allToppings);
+});
 
 app.get('/createPizza/:numToppings/:pizzaName', function (req, res) {
     newPizzaName = req.params.pizzaName;
     pizza.pizzaName = newPizzaName;
     pizza.numToppings = req.params.numToppings;
+    if (pizza.price == 0) {
+        if (pizza.numToppings == 0 || pizza.numToppings == 1) {
+            pizza.price += 6.79;
+        } else {
+            pizza.price += 8.99;
+        }
+    }
     console.log(pizza.pizzaName);
     res.json(JSON.stringify(true));
 });
 
 app.get('/createSetPizza/:numToppings/:pizzaName', function (req, res) {
-    console.log("CreatingPiza")
+    console.log("CreatingPizza")
     newPizzaName = JSON.stringify(req.params.pizzaName);
     pizza.pizzaName = newPizzaName;
     pizza.numToppings = req.params.numToppings;
+    pizza.price = 6.79;
     if (req.params.numToppings == "1") {
         pizza.toppings.push("Pepperoni");
     }
@@ -158,8 +177,23 @@ app.get('/addTopping/:toppingName', function (req, res) {
     if (pizza.currToppings == pizza.numToppings) {
         console.log("false");
         res.json("false");
-    } else {
-        console.log("true");
+    } else if (pizza.numToppings == 1 && req.params.toppingName == 'Pepperoni') {
+        pizza.toppings.push(req.params.toppingName);
+        pizza.currToppings++;
+        pizza.price = 6.79
+        res.json("true");
+    }
+    else if (pizza.numToppings == 1) {
+        if (req.params.toppingName.length == 0) {
+            pizza.price = 6.79;
+            res.json("true");
+        }
+        pizza.price = 7.79;
+        pizza.toppings.push(req.params.toppingName);
+        pizza.currToppings++;
+        res.json("true");
+    }
+    else {
         pizza.toppings.push(req.params.toppingName);
         pizza.currToppings++;
         res.json("true");
@@ -167,14 +201,23 @@ app.get('/addTopping/:toppingName', function (req, res) {
 });
 
 app.get('/removeLastTopping', function (req, res) {
-    pizza.toppings.pop();
+    if (pizza.currToppings != 0) {
+        pizza.currToppings--;
+        pizza.toppings.pop();
+        if (pizza.numToppings == 1 || pizza.numToppings == 0) {
+            pizza.price = 6.79;
+        }
+
+    }
     res.json(JSON.stringify(true));
 });
 
 app.get('/addToOrder', function (req, res) {
     const pushPizza = structuredClone(pizza);
-    pizzaList.push(pushPizza);
-    refreshPizza();
+    if (pizza.pizzaName != "") {
+        pizzaList.push(pushPizza);
+        refreshPizza();
+    }
     res.json(JSON.stringify(true));
 });
 
@@ -185,14 +228,18 @@ app.get('/addSauce/:sauceName', function (req, res) {
 });
 
 app.get('/crustType/:crustToggle', function (req, res) {
-    //TODO, make sure this is toggleable
     pizza.isCauly = req.params.crustToggle;
-    console.log(pizza.isCauly);
     res.json(JSON.stringify(true));
 });
-app.get('comboMeal', function (req, res) {
-    pizza.isCombo = drinkCombo;
-    res.json(JSON.stringify(true));
+
+app.get('/comboMeal/:thisCombo', function (req, res) {
+    pizza.isCombo = req.params.thisCombo;
+    res.json("true");
+});
+
+app.get("/soloDrink", function (req, res) {
+    numDrinks++;
+    res.json("true");
 });
 
 app.get('/calculatePrice', function (req, res) {
@@ -202,8 +249,9 @@ app.get('/calculatePrice', function (req, res) {
         if (currentPizza.numToppings == -1) {
             price += 2.45;
         }
-        else if (currentPizza.numToppings == 0 || (currentPizza.numToppings == 1 && currentPizza.toppings[0] == 'Pepperoni')) {
-            if (currentPizza.isCombo) {
+        else if (currentPizza.numToppings == 0 || (currentPizza.numToppings == 1 && currentPizza.toppings[0] == 'Pepperoni')
+            || (currentPizza.numToppings == 1 && currentPizza.toppings.length == 0)) {
+            if (currentPizza.isCombo == "true") {
                 price += 7.99;
             }
             else {
@@ -211,7 +259,7 @@ app.get('/calculatePrice', function (req, res) {
             }
         }
         else if (currentPizza.numToppings == 1) {
-            if (currentPizza.isCombo) {
+            if (currentPizza.isCombo == "true") {
                 price += 10.24;
             }
             else {
@@ -219,27 +267,31 @@ app.get('/calculatePrice', function (req, res) {
             }
         }
         else {
-            if (currentPizza.isCombo) {
+            if (currentPizza.isCombo == "true") {
                 price += 11.44;
             }
             else {
                 price += 8.99;
             }
         }
-        if (currentPizza.isCauly) {
+        if (currentPizza.isCauly == "true") {
             price += 2.99;
         }
     }
-    //CHECK: might have to stringify this
-    res.json(JSON.stringify(price));
+    price += (numDrinks * 2.45);
+    price = price.toFixed(2);
+    res.json(price);
 });
 
 app.get('/cancelOrder', function (req, res) {
     refreshPizza();
     pizzaList = [];
+    numDrinks = 0;
+    res.json(true);
 });
 app.get('/clearSelection', function (req, res) {
     pizzaList.pop();
+    res.json(true);
 });
 
 app.get('/deletePizza', function (req, res) {
@@ -251,6 +303,7 @@ app.get('/deletePizza', function (req, res) {
     pizza.toppings = [];
     pizza.isCauly = false;
     pizza.isCombo = false;
+    pizza.price = 0;
 });
 
 function refreshPizza() {
@@ -262,23 +315,109 @@ function refreshPizza() {
     pizza.toppings = [];
     pizza.isCauly = false;
     pizza.isCombo = false;
+    pizza.price = 0;
 }
 
 app.get('/checkoutScreen', function (req, res) {
-    let completeOrder = 'Order Info: \n';
-    for (let i = 0; i < pizzaList.length(); i++) {
-        completeOrder += pizza.name;
-        completeOrder += "- ";
-        for (let j = 0; i < pizza.toppings.length(); j++) {
-            completeOrder += pizza.toppings[j];
+    var completeOrder = "";
+    completeOrder += "Order Info: ";
+    for (let i = 0; i < pizzaList.length; i++) {
+        tempPizza = pizzaList[i]
+        completeOrder += "~Pizza Type: " + tempPizza.pizzaName;
+        let price = tempPizza.price
+        if (tempPizza.isCombo == "true") {
+            completeOrder += "~With added fountain drink"
+            if (tempPizza.numToppings == 1 && tempPizza.toppings[0] == "Pepperoni" || tempPizza.numToppings == 1 && tempPizza.toppings.length == 0) {
+                price += 1.20
+            }
+            else {
+                price += 2.45
+            }
+        }
+        for (let j = -1; j < tempPizza.toppings.length; j++) {
+            if (j == -1) {
+                completeOrder = completeOrder + "~Sauce: " + tempPizza.sauce + "~Cheese: House Blend "
+                if (tempPizza.isCauly == "true") {
+                    completeOrder += "~Crust: Cauliflower ";
+                }
+                else {
+                    completeOrder += "~Crust: Standard Dough ";
+                }
+                completeOrder += "~Toppings: "
+            } else {
+                completeOrder += tempPizza.toppings[j];
+            }
             completeOrder += " ";
         }
-        completeOrder += ("\n");
+
+
+        if (tempPizza.isCauly == "true") {
+            price += 2.99;
+        }
+
+        completeOrder += ("~Price: $");
+        price = price.toFixed(2);
+        completeOrder += price;
+
+    }
+
+    if (numDrinks > 0) {
+        completeOrder += "~" + numDrinks + " Added Fountain Drinks: "
+        let price = (numDrinks * 2.45);
+        price = price.toFixed(2);
+        completeOrder += String(price) + " total";
     }
     //MIGHT have to stringify this
     res.json(completeOrder)
 });
 
+app.get('/currentPizza', function (req, res) {
+    let thisPizza = 'Pizza Info: ';
+    makePizza = pizza;
+    thisPizza += makePizza.pizzaName;
+    thisPizza += " ";
+    thisPizza = thisPizza + " Sauce: " + makePizza.sauce + " ";
+    if (makePizza.isCauly == "true") {
+        thisPizza += "Crust: Cauliflower ";
+    }
+    else {
+        thisPizza += "Crust: Standard Dough ";
+    }
+    thisPizza += "Toppings: House Blend Cheese, "
+    for (let i = 0; i < makePizza.currToppings; i++) {
+        thisPizza = thisPizza + makePizza.toppings[i] + " ";
+    }
+    thisPizza += " | "
+    var price = makePizza.price;
+    if (makePizza.isCauly == "true") {
+        price += 2.99;
+    }
+    if (makePizza.isCombo == "true") {
+        thisPizza += " With Added Fountain Drink. | "
+        if (makePizza.numToppings == 0 || (makePizza.numToppings == 1 && makePizza.toppings[0] == "Pepperoni") ||
+            (makePizza.toppings.length == 0 && makePizza.numToppings == 1)) {
+            price += 1.20;
+        }
+        else {
+            price += 2.45;
+        }
+    }
+
+    thisPizza += ("Price: $");
+    price = price.toFixed(2);
+    thisPizza += price;
+    res.json(thisPizza);
+});
+
+//Make the pool for later use
+const pool = new Pool({
+    user: process.env.PSQL_USER,
+    host: process.env.PSQL_HOST,
+    database: process.env.PSQL_DATABASE,
+    password: process.env.PSQL_PASSWORD,
+    port: process.env.PSQL_PORT,
+    ssl: { rejectUnauthorized: false }
+});
 
 app.put("/checkoutServ", async (req, res) => {
 
@@ -300,12 +439,8 @@ app.put("/checkoutServ", async (req, res) => {
 })
 
 app.get("/seasonalMenu", async (req, res) => {
-    var seasonalReply = await pool.query('SELECT * FROM INVENTORY WHERE id > 48');
+    var seasonalReply = await pool.query('SELECT name FROM INVENTORY WHERE id > 48');
     return res.json(seasonalReply.rows);
-})
-
-app.put("/serverCheckout", async (req, res) => {
-    
 })
 
 //exit gracefully
@@ -315,65 +450,4 @@ process.on('SIGINT', function () {
     process.exit(0);
 });
 
-
-
-// // //Black magic API testing
-// function initMap() {
-//     var directionsService = new google.maps.DirectionsService();
-//     var directionsRenderer = new google.maps.DirectionsRenderer();
-//     const map = new google.maps.Map(document.getElementById("map"), {
-//         zoom: 16,
-//         center: { lat: 30.61234195012257, lng: -96.34153287461642 },
-//     });
-//     directionsRenderer.setMap(map);
-// }
-
-// function calcRoute() {
-//     var start = document.getElementById('start').value;
-//     var request = {
-//         origin: start,
-//         destination: { lat: 30.61234195012257, lng: -96.34153287461642 },
-//         travelMode: 'DRIVING'
-//     };
-//     directionsService.route(request, function (result, status) {
-//         if (status == 'OK') {
-//             directionsRenderer.setDirections(result);
-//         }
-//     });
-// }
-
-// //Black magic API testing
-app.get('/initMap/:address', function (req, res) {
-    var directionsService = new google.maps.DirectionsService();
-    var directionsRenderer = new google.maps.DirectionsRenderer();
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 16,
-        center: { lat: 30.61234195012257, lng: -96.34153287461642 },
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-    });
-    directionsRenderer.setMap(map);
-    var request = {
-        origin: "125 Spence St, College Station, TX 77840",
-        destination: { lat: 30.61234195012257, lng: -96.34153287461642 },
-        travelMode: 'DRIVING'
-    };
-    directionsService.route(request, function (result, status) {
-        if (status == 'OK') {
-            directionsRenderer({
-                suppressMarkers: true,
-                directions: result,
-                map: map,
-            });
-
-        }
-    });
-});
-
-
-process.on('SIGINT', function() {
-    pool.end();
-    console.log('Application successfully shutdown');
-    process.exit(0);
-});
-
-module.exports = {  };
+module.exports = {};
